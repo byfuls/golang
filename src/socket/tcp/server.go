175d78734862203ac91d9fbdfb647de1a05e5471
byfuls <byfuls@gmail.com>
2020-10-21 18:08:11 +0900

    (20201021,byfuls) go tour :)

---
+++ src/socket/tcp/server.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	//"bufio"
+	"os"
+)
+
+var ADDRESS = "127.0.0.1:10000"
+
+func echo(client net.Conn) {
+	// --1
+	//buf := bufio.NewReader(client)
+	//for {
+	//	rbuf, err := buf.ReadBytes(byte('\n'))
+	//	if err != nil {
+	//		fmt.Println("read err: ", err)
+	//		return
+	//	}
+	//	fmt.Println(rbuf)
+	//	client.Write(rbuf)
+	//}
+
+	// --2
+	buf := make([]byte, 1024)
+	for {
+		size, err := client.Read(buf)
+		if err != nil || size == 0 {
+			fmt.Println("read err: ", err)
+			return
+		}
+		fmt.Println(buf)
+		client.Write(buf)
+	}
+}
+
+func main() {
+	sock, err := net.Listen("tcp", ADDRESS)
+	if err != nil {
+		fmt.Println("server listen err: ", err)
+		os.Exit(1)
+	}
+
+	for {
+		client, err := sock.Accept()
+		if err != nil {
+			fmt.Println("server accept err: ", err)
+			continue
+		}
+
+		go echo(client)
+	}
+}
