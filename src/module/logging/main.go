265802d3f4c3c818a2c57e011670595ef5d6d797
byfuls <byfuls@gmail.com>
2020-10-28 12:04:42 +0900

    (20201028,byfuls) modified

--- src/module/logging/main.go
+++ src/module/logging/main.go
@@ -1,7 +1,7 @@
-package main
+package logging
 
 import (
-	"fmt"
+	//"fmt"
 	"log"
 	"os"
 	"sync"
@@ -22,6 +22,7 @@ type loggingManager struct {
 	info  *log.Logger
 	warn  *log.Logger
 	error *log.Logger
+	debug *log.Logger
 }
 
 var loggingMgr *loggingManager
@@ -32,23 +33,26 @@ func (l *loggingManager) attachFp() {
 		l.file.Close()
 	}
 
-	fmt.Printf("[attach/fullPath] %s\n", l.fullPath)
+	//fmt.Printf("[attach/fullPath] %s\n", l.fullPath)
 	fp, err := os.OpenFile(l.fullPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
 		panic(err)
 	}
 
 	l.file = fp
-	l.trace = log.New(loggingMgr.file, "[TRACE]   ", log.Ldate|log.Ltime|log.Lmicroseconds|log.Lshortfile)
-	l.info = log.New(loggingMgr.file, "[INFO]    ", log.Ldate|log.Ltime|log.Lmicroseconds|log.Lshortfile)
-	l.warn = log.New(loggingMgr.file, "[WARNING] ", log.Ldate|log.Ltime|log.Lmicroseconds|log.Lshortfile)
-	l.error = log.New(loggingMgr.file, "[ERROR]   ", log.Ldate|log.Ltime|log.Lmicroseconds|log.Lshortfile)
+	/* log.Llongfile || log.Lshortfile */
+	l.debug = log.New(loggingMgr.file, "[DEBUG] ", log.Ldate|log.Ltime|log.Lmicroseconds)
+	l.trace = log.New(loggingMgr.file, "[TRACE] ", log.Ldate|log.Ltime|log.Lmicroseconds)
+	l.info = log.New(loggingMgr.file, "[INFO] ", log.Ldate|log.Ltime|log.Lmicroseconds)
+	l.warn = log.New(loggingMgr.file, "[WARN]  ", log.Ldate|log.Ltime|log.Lmicroseconds)
+	l.error = log.New(loggingMgr.file, "[ERROR] ", log.Ldate|log.Ltime|log.Lmicroseconds)
+
 }
 
 func (l *loggingManager) getToday() (string, bool) {
 	date := time.Now().Format("20060102")
 
-	fmt.Printf("[%s] [%s]\n", l.date, date)
+	//fmt.Printf("[%s] [%s]\n", l.date, date)
 	if l.date != date {
 		return date, true
 	}
@@ -67,59 +71,67 @@ func (l *loggingManager) prep() {
 }
 
 /**********************************************************************/
-/*
-traceF
-traceLn
-infoF
-infoLn
-warnF
-warnLn
-errorF
-errorLn
-*/
+//	traceF
+//	traceLn
+//	infoF
+//	infoLn
+//	warnF
+//	warnLn
+//	errorF
+//	errorLn
 
-func traceF(format string, v ...interface{}) {
+func DebugF(format string, v ...interface{}) {
+	loggingMgr.prep()
+	loggingMgr.debug.Printf(format, v...)
+}
+
+func DebugLn(v ...interface{}) {
+	loggingMgr.prep()
+	loggingMgr.debug.Println(v...)
+}
+
+func TraceF(format string, v ...interface{}) {
 	loggingMgr.prep()
 	loggingMgr.trace.Printf(format, v...)
 }
 
-func traceLn(v ...interface{}) {
+func TraceLn(v ...interface{}) {
 	loggingMgr.prep()
 	loggingMgr.trace.Println(v...)
 }
 
-func infoF(format string, v ...interface{}) {
+func InfoF(format string, v ...interface{}) {
 	loggingMgr.prep()
 	loggingMgr.info.Printf(format, v...)
 }
 
-func infoLn(v ...interface{}) {
+func InfoLn(v ...interface{}) {
 	loggingMgr.prep()
 	loggingMgr.info.Println(v...)
 }
 
-func warnF(format string, v ...interface{}) {
+func WarnF(format string, v ...interface{}) {
 	loggingMgr.prep()
 	loggingMgr.warn.Printf(format, v...)
 }
 
-func warnLn(v ...interface{}) {
+func WarnLn(v ...interface{}) {
 	loggingMgr.prep()
 	loggingMgr.warn.Println(v...)
 }
 
-func errorF(format string, v ...interface{}) {
+func ErrorF(format string, v ...interface{}) {
 	loggingMgr.prep()
 	loggingMgr.error.Printf(format, v...)
 }
 
-func errorLn(v ...interface{}) {
+func ErrorLn(v ...interface{}) {
 	loggingMgr.prep()
 	loggingMgr.error.Println(v...)
 }
 
-func Init(base string) bool {
-	if 0 >= len(base) {
+func Init(base string, fileName string) bool {
+	if 0 >= len(base) || 0 >= len(fileName) {
 		return false
 	}
 
@@ -128,42 +140,41 @@ func Init(base string) bool {
 
 	directory := base
 	date := time.Now().Format("20060102")
-	file := "loggingTest"
 	fullPath := base + "/" + date
 
-	fmt.Println(fullPath)
+	//fmt.Println(fullPath)
 	if err := os.MkdirAll(fullPath, 0755); err != nil {
 		return false
 	}
 
 	loggingMgr.directory = directory
 	loggingMgr.date = date
-	loggingMgr.fileName = file
-	loggingMgr.fullPath = fullPath + "/" + file
-	fmt.Println(loggingMgr.fullPath)
-	fmt.Printf("[full path] %s\n", loggingMgr.fullPath)
+	loggingMgr.fileName = fileName
+	loggingMgr.fullPath = fullPath + "/" + fileName
+	//fmt.Println(loggingMgr.fullPath)
+	//fmt.Printf("[full path] %s\n", loggingMgr.fullPath)
 
 	loggingMgr.attachFp()
 
 	return true
 }
 
-func main() {
-	baseDir := os.Getenv("LOG_DIRECTORY")
-	fmt.Println(baseDir)
-
-	if !Init(baseDir) {
-		return
-	}
-
-	traceLn("HI")
-	infoLn("HI")
-	warnLn("HI")
-	errorLn("HI")
-
-	test := 1
-	traceF("%s:%d\n", "TRACE TEST", test)
-	infoF("%s:%d\n", "INFO TEST", test)
-	warnF("%s:%d\n", "WARN TEST", test)
-	errorF("%s:%d\n", "ERROR TEST", test)
-}
+//func main() {
+//	baseDir := os.Getenv("LOG_DIRECTORY")
+//	fmt.Println(baseDir, "logfile")
+//
+//	if !Init(baseDir) {
+//		return
+//	}
+//
+//	traceLn("HI")
+//	infoLn("HI")
+//	warnLn("HI")
+//	errorLn("HI")
+//
+//	test := 1
+//	traceF("%s:%d\n", "TRACE TEST", test)
+//	infoF("%s:%d\n", "INFO TEST", test)
+//	warnF("%s:%d\n", "WARN TEST", test)
+//	errorF("%s:%d\n", "ERROR TEST", test)
+//}
