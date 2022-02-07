a3e44d33f062c890ffa3a7c1f57450d7a9e1646d
byfuls <byfuls@gmail.com>
2022-02-07 15:52:21 +0900

    update

--- src/http/jwt/main.go
+++ src/http/jwt/main.go
@@ -3,6 +3,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
@@ -41,7 +42,100 @@ func init() {
 
 func main() {
 	router.POST("/login", Login)
+	router.POST("/logout", TokenAuthMiddleware(), Logout)
+	router.POST("/token/refresh", Refresh)
+	router.POST("/todo", TokenAuthMiddleware(), CreateTodo)
+	// router.POST("/logout", Logout)
+	// router.POST("/todo", CreateTodo)
+
 	log.Fatal(router.Run(":2222"))
+}
+
+func TokenAuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		err := TokenValid(c.Request)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, err.Error())
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
+}
+
+func Refresh(c *gin.Context) {
+	mapToken := map[string]string{}
+	if err := c.ShouldBindJSON(&mapToken); err != nil {
+		c.JSON(http.StatusUnprocessableEntity, err.Error())
+		return
+	}
+	refreshToken := mapToken["refresh_token"]
+
+	// verify the token
+	os.Setenv("REFRESH_SECRET", "mcmvmkmsdnfsdmfdsjf") // this should be in an env file
+	token, err := jwt.Parse(refreshToken, func(token *jwt.Token) (interface{}, error) {
+		// make sure that the token method conform to "SigningMethodHMAC"
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(os.Getenv("REFRESH_SECRET")), nil
+	})
+
+	// if there is an error, the token must have expired
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, "Refresh token expired")
+		return
+	}
+
+	// is token valid?
+	if _, ok := token.Claims.(jwt.Claims); !ok && !token.Valid {
+		c.JSON(http.StatusUnauthorized, err)
+		return
+	}
+
+	// Since token is valid, get the uuid:
+	claims, ok := token.Claims.(jwt.MapClaims) // the token claims should conform to MapClaims
+	if ok && token.Valid {
+		refreshUuid, ok := claims["refresh_uuid"].(string) // convert the interface to string
+		if !ok {
+			c.JSON(http.StatusUnprocessableEntity, err)
+			return
+		}
+		userId, err := strconv.ParseUint(fmt.Sprintf("%.f", claims["user_id"]), 10, 64)
+		if err != nil {
+			c.JSON(http.StatusUnprocessableEntity, "Error occurred")
+			return
+		}
+
+		// Delete the previous Refresh Token
+		delErr := DeleteAuth(refreshUuid)
+		if delErr != nil {
+			c.JSON(http.StatusUnauthorized, "unauthorized")
+			return
+		}
+
+		// Create new pairs of refresh and access tokens
+		ts, createErr := CreateToken(userId)
+		if createErr != nil {
+			c.JSON(http.StatusForbidden, createErr.Error())
+			return
+		}
+
+		// save the tokens metadata to redis
+		saveErr := CreateAuth(userId, ts)
+		if saveErr != nil {
+			c.JSON(http.StatusForbidden, saveErr.Error())
+			return
+		}
+
+		tokens := map[string]string{
+			"access_token":  ts.AccessToken,
+			"refresh_token": ts.RefreshToken,
+		}
+		c.JSON(http.StatusCreated, tokens)
+	} else {
+		c.JSON(http.StatusUnauthorized, "refresh expired")
+	}
 }
 
 type User struct {
@@ -65,6 +159,7 @@ func Login(c *gin.Context) {
 		return
 	}
 
+	// compare the user from the request, with the one we defined
 	if user.Username != u.Username || user.Password != u.Password {
 		c.JSON(http.StatusUnauthorized, "Please provide valid login details")
 		return
@@ -87,6 +182,20 @@ func Login(c *gin.Context) {
 	c.JSON(http.StatusOK, tokens)
 }
 
+func Logout(c *gin.Context) {
+	au, err := ExtractTokenMetadata(c.Request)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, "unauthorized")
+		return
+	}
+	err = DeleteAuth(au.AccessUuid)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, "unauthorized")
+		return
+	}
+	c.JSON(http.StatusOK, "Successfully logged out")
+}
+
 type TokenDetails struct {
 	AccessToken  string
 	RefreshToken string
@@ -99,6 +208,7 @@ type TokenDetails struct {
 func CreateToken(userId uint64) (*TokenDetails, error) {
 	td := &TokenDetails{}
 	td.AtExpires = time.Now().Add(time.Minute * 15).Unix()
+	// td.AtExpires = time.Now().Add(time.Second * 5).Unix()
 	td.AccessUuid = uuid.NewV4().String()
 
 	td.RtExpires = time.Now().Add(time.Hour * 24 * 7).Unix()
@@ -133,16 +243,17 @@ func CreateToken(userId uint64) (*TokenDetails, error) {
 	return td, nil
 }
 
+type RedisMapAccessTokenSaveFormat struct {
+	UserId string
+	ExTime time.Duration
+}
+
 func CreateAuth(userId uint64, td *TokenDetails) error {
 	at := time.Unix(td.AtExpires, 0) // converting Unix to UTC
 	rt := time.Unix(td.RtExpires, 0)
 	now := time.Now()
 
 	// set uuid, userid, now
-	type RedisMapAccessTokenSaveFormat struct {
-		UserId string
-		ExTime time.Duration
-	}
 	redis[td.AccessUuid] = RedisMapAccessTokenSaveFormat{
 		UserId: strconv.Itoa(int(userId)),
 		ExTime: at.Sub(now),
@@ -151,6 +262,12 @@ func CreateAuth(userId uint64, td *TokenDetails) error {
 		UserId: strconv.Itoa(int(userId)),
 		ExTime: rt.Sub(now),
 	}
+	return nil
+}
+
+func DeleteAuth(givenUuid string) error {
+	// deleted, err := client.Del(givenUuid).Result()
+	delete(redis, givenUuid)
 	return nil
 }
 
@@ -181,8 +298,94 @@ func VerifyToken(r *http.Request) (*jwt.Token, error) {
 		}
 		return []byte(os.Getenv("ACCESS_SECRET")), nil
 	})
+	// DEBUG DEBUG DEBUG - START
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		fmt.Println("[Debug] debug fail, result: ", err)
+	} else {
+		fmt.Println(claims)
+		fmt.Println("[Debug] debug ok, result: ", err)
+	}
+	// DEBUG DEBUG DEBUG - END
 	if err != nil {
 		return nil, err
 	}
+
 	return token, nil
+}
+
+// token 만료 검사
+func TokenValid(r *http.Request) error {
+	token, err := VerifyToken(r)
+	if err != nil {
+		return err
+	}
+	if _, ok := token.Claims.(jwt.Claims); !ok && !token.Valid {
+		return err
+	}
+	return nil
+}
+
+type AccessDetails struct {
+	AccessUuid string
+	UserId     uint64
+}
+
+// token 추출 및 조회
+func ExtractTokenMetadata(r *http.Request) (*AccessDetails, error) {
+	token, err := VerifyToken(r)
+	if err != nil {
+		return nil, err
+	}
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if ok && token.Valid {
+		accessUuid, ok := claims["access_uuid"].(string)
+		if !ok {
+			return nil, err
+		}
+		userId, err := strconv.ParseUint(fmt.Sprintf("%.f", claims["user_id"]), 10, 64)
+		if err != nil {
+			return nil, err
+		}
+		return &AccessDetails{
+			AccessUuid: accessUuid,
+			UserId:     userId,
+		}, nil
+	}
+	return nil, err
+}
+
+// find token in database(=redis) via uuid
+func FetchAuth(authD *AccessDetails) (uint64, error) {
+	// userId, err := client.Get(authD.AccessUuid).Result()
+	userId, exists := redis[authD.AccessUuid]
+	if !exists {
+		return 0, errors.New("not exists")
+	}
+	fmt.Println("[DEBUG] ", userId.(RedisMapAccessTokenSaveFormat).UserId)
+	userID, _ := strconv.ParseUint(userId.(RedisMapAccessTokenSaveFormat).UserId, 10, 64)
+	return userID, nil
+}
+
+func CreateTodo(c *gin.Context) {
+	var td *Todo
+	if err := c.ShouldBindJSON(&td); err != nil {
+		c.JSON(http.StatusUnprocessableEntity, "invalid json")
+		return
+	}
+	tokenAuth, err := ExtractTokenMetadata(c.Request)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, "unauthorized")
+		return
+	}
+	userId, err := FetchAuth(tokenAuth)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, "unauthorized")
+		return
+	}
+	td.UserID = userId
+
+	//you can proceed to save the Todo to a database
+	//but we will just return it to the caller here:
+	c.JSON(http.StatusCreated, td)
 }
